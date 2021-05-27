893a4bed6c79b3f6a8363a2fb49eaaa5362dfe21
Kathurima Kimathi <kathurima@healthcloud.co.ke>
2021-05-27 22:35:39 +0300

    add lets-encrypt role
    Signed-off-by: Kathurima Kimathi <kathurima@healthcloud.co.ke>

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from fabric.api import *
 
 def deploy():
-    local('ansible-playbook -i hosts.yml lets-encryprt.yml')
-    #local('ansible-playbook -i hosts.yml playbook.yml')
+    local('ansible-playbook -i hosts.yml playbook.yml')
+    local('ansible-playbook -i hosts.yml lets-encrypt.yml')
